4bc9ef6f8885e96e64f44b995534cd4850c1b8d4
Rafael Cabral <rafaelbkbral@gmail.com>
2022-08-24 22:46:59 -0300

    app simples flask

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-print("Ola pessoas!")
+from flask import Flask
 
-if 1 == 2:
-    print("eu sou ruim de matematica!")
-else:
-    print("explicando")
+app = Flask(__name__)
+
+@app.route("/ola")
+def hello_world():
+    return "Hello World"
+
+@app.route("/soma")
+def soma_um_mais_um():
+    soma = 1+1
+    return str(soma)
+
+@app.route("/pessoa")
+def dados_pessoa():
+    nome = "rafael"
+    sobrenome = "cabral"
+    idade = "18 anos"
+    return [{
+        'nome': nome,
+        'sobrenome': sobrenome,
+        'idade': "18 anos"
+    },
+    {
+        'nome': "alan",
+        'sobrenome': "Oliveira",
+        'idade': "16 anos"
+    }
+    ]
